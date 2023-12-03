ef2b1c496c7e9992c62bc931be8481fea835096b
luana-gruber <luana_gruber@hotmail.com>
2023-12-03 00:37:55 -0300

    bug-fix adição de quantidade quando o cupcake já está no carrinho

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,6 +8,17 @@ if (localStorage.getItem("Carrinho" != null)){
 btnComprar.forEach(item => {
     item.addEventListener('click', () =>
         {
+            var indexCarrinho1 = recuperaCarrinhoCupcakes.cupcakes.findIndex(cupcake => cupcake.id == item.getAttribute("data-id"));
+            let arrayCupcakesSalvos1 = recuperaCarrinhoCupcakes.cupcakes.filter(cupcake => (cupcake.id === item.getAttribute("data-id")))
+            console.log(indexCarrinho1)
+            console.log(arrayCupcakesSalvos1)
+            if (arrayCupcakesSalvos1.length > 0){
+                console.log(indexCarrinho1)
+                console.log(arrayCupcakesSalvos1)
+                recuperaCarrinhoCupcakes.cupcakes[indexCarrinho1].qnt += 1
+                localStorage.setItem("Carrinho", JSON.stringify(recuperaCarrinhoCupcakes))
+            }
+            else{
             let cupcake = {
                 id: item.getAttribute("data-id"),
                 img: item.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.children[0].getAttribute("src"),
@@ -17,5 +28,6 @@ btnComprar.forEach(item => {
             }
             recuperaCarrinhoCupcakes.cupcakes.push(cupcake)
             localStorage.setItem("Carrinho", JSON.stringify(recuperaCarrinhoCupcakes))
+        }
         })
 });
