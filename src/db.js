c72a6646b8af7e49aebf3f40dc4e2788e24ea6f5
luana-gruber <luana_gruber@hotmail.com>
2023-12-03 09:09:39 -0300

    Remoção connection limit

--- src/db.js
+++ src/db.js
@@ -8,9 +8,7 @@ async function conecta(){
         user: process.env.DB_USERNAME, 
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DBNAME,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
+        waitForConnections: true
     })
     global.connection = conn
     return connection
