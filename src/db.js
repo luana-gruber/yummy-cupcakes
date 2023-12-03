b588531c1617c3704d61ff5fcd1451408222763b
luana-gruber <luana_gruber@hotmail.com>
2023-12-03 15:28:39 -0300

    alteração variáveis conexão mysql

--- src/db.js
+++ src/db.js
@@ -4,10 +4,10 @@ const mysqlSession = require("express-mysql-session")(session)
 async function conecta(){
     const mysql = require("mysql2/promise")
     const conn = await mysql.createConnection({
-        host: process.env.DB_HOST, 
-        user: process.env.DB_USERNAME, 
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DBNAME,
+        host: "mysql-156527-0.cloudclusters.net", 
+        user: "admin", 
+        password: "1E3GHXaZ",
+        database: "yummy",
         expiration: 3600000,
         waitForConnections: true
     })
